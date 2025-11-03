2e0fb8a5b14a5c07db130f72f065205929119ef5
mvladt <v-mell@yandex.ru>
2025-11-03 13:49:46 +0300

    Поправил 'vite.config.ts'

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import mkcert from "vite-plugin-mkcert";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    // mkcert()
-  ],
-  build: {
-    outDir: "docs",
-  },
-  server: {
-    // proxy: {
-    //   "/api": {
-    //     target: "https://scheduler.push.mvladt.ru",
-    //     changeOrigin: true,
-    //   },
-    // },
-    // https: true,
-  },
+  plugins: [react()],
 });
